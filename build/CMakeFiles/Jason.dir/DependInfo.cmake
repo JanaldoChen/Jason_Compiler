93924e2bb12286377e2b3bd3a03955706c2aafda
JanaldoChen <2032589713@qq.com>
2018-12-12 18:29:17 +0800

    Add the build

---
+++ build/CMakeFiles/Jason.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/mac/Desktop/Jason/Jason.c" "/Users/mac/Desktop/Jason/build/CMakeFiles/Jason.dir/Jason.c.o"
+  "/Users/mac/Desktop/Jason/object.c" "/Users/mac/Desktop/Jason/build/CMakeFiles/Jason.dir/object.c.o"
+  "/Users/mac/Desktop/Jason/quad.c" "/Users/mac/Desktop/Jason/build/CMakeFiles/Jason.dir/quad.c.o"
+  "/Users/mac/Desktop/Jason/scan.c" "/Users/mac/Desktop/Jason/build/CMakeFiles/Jason.dir/scan.c.o"
+  "/Users/mac/Desktop/Jason/symbol.c" "/Users/mac/Desktop/Jason/build/CMakeFiles/Jason.dir/symbol.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Jason.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jason.dir/Jason.c.o"
+  "CMakeFiles/Jason.dir/scan.c.o"
+  "CMakeFiles/Jason.dir/symbol.c.o"
+  "CMakeFiles/Jason.dir/quad.c.o"
+  "CMakeFiles/Jason.dir/object.c.o"
+  "Jason.pdb"
+  "Jason"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Jason.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
